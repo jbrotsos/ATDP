9b0188bcd714900163d55b28d471fa9aaccd9eac
james <jayme.brotsos@gmail.com>
2015-05-10 17:36:58 -0700

    Client program

---
+++ misc_src/client_server/Client.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        String serverHostname = new String (args[0]);
+
+        if (args.length > 0)
+           serverHostname = args[0];
+        System.out.println ("Attemping to connect to host " +
+		serverHostname + " on port 10007.");
+
+        Socket echoSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            echoSocket = new Socket(serverHostname, 10007);
+            out = new PrintWriter(echoSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                                        echoSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: " + serverHostname);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for "
+                               + "the connection to: " + serverHostname);
+            System.exit(1);
+        }
+
+	BufferedReader stdIn = new BufferedReader(
+                                   new InputStreamReader(System.in));
+	String userInput;
+
+        System.out.print ("input: ");
+	while ((userInput = stdIn.readLine()) != null) {
+	    out.println(userInput);
+	    System.out.println("echo: " + in.readLine());
+            System.out.print ("input: ");
+	}
+
+	out.close();
+	in.close();
+	stdIn.close();
+	echoSocket.close();
+    }
+}
